8fe0732bb3182449d1088c5139bbba5d31c0ae54
gze1206 <gze1206@naver.com>
2024-05-10 17:30:58 +0900

    feat: Add prefix to player display name temporary

--- src/main/java/net/gze1206/plugin/core/UserManager.kt
+++ src/main/java/net/gze1206/plugin/core/UserManager.kt
@@ -2,7 +2,10 @@ package net.gze1206.plugin.core
 
 import net.gze1206.plugin.Main
 import net.gze1206.plugin.model.User
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
 import org.bukkit.entity.Player
 import java.util.UUID
 
@@ -11,8 +14,11 @@ class UserManager {
         private var activeUsers = HashMap<UUID, User>()
 
         fun addPlayer(player: Player) : User {
-            val displayName = player.displayName() as TextComponent
-            val user = User(player.uniqueId, displayName.content(), null, 0UL)
+            val displayName = (player.displayName() as TextComponent).content()
+            player.displayName(
+                Component.text("[테스트] ").color(TextColor.color(0x13f832))
+                .append(Component.text(displayName, NamedTextColor.WHITE)))
+            val user = User(player.uniqueId, displayName, null, 0UL)
             activeUsers[player.uniqueId] = user
 
             Main.instance!!.logger.info("플레이어 ${player.name} 입장")

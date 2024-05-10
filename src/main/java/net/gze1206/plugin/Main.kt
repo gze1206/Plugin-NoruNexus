12133e5a1d5a337cb1d48606d4e2cc6c2221d69d
gze1206 <gze1206@naver.com>
2024-05-10 15:57:38 +0900

    feat: Apply Kotlin

--- src/main/java/net/gze1206/plugin/Main.kt
+++ src/main/java/net/gze1206/plugin/Main.kt
@@ -1,16 +1,13 @@
-package net.gze1206.plugin;
+package net.gze1206.plugin
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin
 
-public final class Plugin extends JavaPlugin {
-
-    @Override
-    public void onEnable() {
-        getLogger().info("플러그인 활성화!");
+class Main : JavaPlugin() {
+    override fun onEnable() {
+        logger.info("플러그인 활성화!")
     }
 
-    @Override
-    public void onDisable() {
-        getLogger().info("플러그인 비활성화");
+    override fun onDisable() {
+        logger.info("플러그인 비활성화")
     }
 }

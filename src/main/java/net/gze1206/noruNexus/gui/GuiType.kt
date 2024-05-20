de8133acc18eb247e4ee5d4ce756a08c95b652f9
gze1206 <gze1206@naver.com>
2024-05-20 23:00:48 +0900

    feat: Clean up code

--- src/main/java/net/gze1206/noruNexus/gui/GuiType.kt
+++ src/main/java/net/gze1206/noruNexus/gui/GuiType.kt
@@ -3,5 +3,4 @@ package net.gze1206.noruNexus.gui
 enum class GuiType {
     TITLE,
     SHOP,
-    RUNE,
 }
--- src/main/java/net/gze1206/noruNexus/gui/RuneWindow.kt
+++ src/main/java/net/gze1206/noruNexus/gui/RuneWindow.kt
@@ -7,31 +7,21 @@ import net.gze1206.noruNexus.utils.component
 import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
-import org.bukkit.event.inventory.InventoryAction
-import org.bukkit.event.inventory.InventoryType
-import org.bukkit.event.inventory.InventoryType.SlotType
-import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.MerchantRecipe
 
-class RuneWindow(private val player: Player) : InventoryActionWindow {
+class RuneWindow(private val player: Player) {
     companion object {
         private val windowTitle = "룬 각인".component(NamedTextColor.DARK_PURPLE)
     }
 
-    private val inventory = Bukkit.createInventory(null, InventoryType.GRINDSTONE, windowTitle)
-    override fun inventory() = inventory
-
-    override fun page() = 1
-
     private val merchant = Bukkit.createMerchant(windowTitle)
 
     fun open() {
-        InventoryWindow.inventoryGuiMap[player.uniqueId] = this
-        update()
+        initMerchant()
         player.openMerchant(merchant, true)
     }
 
-    private fun update() {
+    private fun initMerchant() {
         val recipes = mutableListOf<MerchantRecipe>()
         ConfigManager.rune.getRecipeKeys().forEach {
             val ingredients = ConfigManager.rune.getRecipe(it)
@@ -45,9 +35,5 @@ class RuneWindow(private val player: Player) : InventoryActionWindow {
         }
 
         merchant.recipes = recipes
-    }
-
-    override fun onAction(current: ItemStack?, cursor: ItemStack, rawSlot: Int, slotType: SlotType, action: InventoryAction) : Boolean {
-        return false
     }
 }
